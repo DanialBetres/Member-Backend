705d090c5b1ccf08a1ee0bae9afc0cbd22d37f16
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2022-10-23 03:30:13 -0400

    Types and Data

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const graphqlHttp = require('express-graphql').graphqlHTTP;
 const { buildSchema } = require('graphql');
+
+const Org = require('./models/org')
 
 const app = express();
 
@@ -9,12 +12,23 @@ app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Org {
+            _id: ID!
+            name: String!
+            tiers: [String!]!
+        }
+
+        input OrgInput {
+            name: String!
+            tiers: [String!]!
+        }
+
         type RootQuery {
-            orgs: [String!]!
+            orgs: [Org!]!
         }
 
         type RootMutation {
-            createOrg(name: String): String
+            createOrg(orgInput: OrgInput): Org
         }
 
         schema {
@@ -24,14 +38,49 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         orgs: () => {
-            return ['List', "Of", "Orgs"]
+            return Org.find()
+                .then((orgs) => {
+                    return orgs.map((org) => {
+                        return {
+                            ...org._doc,
+                            _id: org.id
+                        };
+                    })
+                })
+                .catch((err) => {
+                    throw err
+                })
         },
 
         createOrg: (args) => {
-            return args.name
+            const org = new Org({
+                name: args.orgInput.name,
+                tiers: args.orgInput.tiers
+            })
+
+            return org.save()
+                .then((result) => {
+                    return {
+                        ...result._doc,
+                        _id: result.id
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    throw err;
+                })
         }
     },
     graphiql: true
 }));
 
-app.listen(3000);
+const DB_NAME = "member-dev"
+mongoose
+    .connect(`mongodb+srv://Member:HrC6UODWUzGYLClv@cluster0.dbjv1kp.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log('Connected to MongoDB. App running on port 3000.')
+        app.listen(3000)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
---
+++ models/org.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const orgSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    tiers: {
+        type: [{
+            type: String
+        }],
+        require: true
+    }
+});
+
+module.exports = mongoose.model('Org', orgSchema);
