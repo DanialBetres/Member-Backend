67bae30b9b2570c46451772c9c2a12601a965e72
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2022-10-23 02:44:31 -0400

    Schemas and Resolvers

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql').graphqlHTTP;
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello, World!');
-})
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            orgs: [String!]!
+        }
+
+        type RootMutation {
+            createOrg(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        orgs: () => {
+            return ['List', "Of", "Orgs"]
+        },
+
+        createOrg: (args) => {
+            return args.name
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
