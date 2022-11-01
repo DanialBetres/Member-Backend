34473d9d720afff331afecb22cf9e1450ec25613
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2022-11-01 01:45:53 -0400

    First Commit
    Added the "deleteOrg" resolver

--- graphql/resolvers/orgs.js
+++ graphql/resolvers/orgs.js
@@ -1,5 +1,6 @@
 const Org = require('../../models/org');
 const User = require("../../models/user");
+const Membership = require("../../models/membership");
 
 const { transformOrg } = require('./merge');
 
@@ -19,8 +20,6 @@ module.exports = {
             throw new Error('Unauthenticated');
         }
 
-
-
         const org = new Org({
             name: args.orgInput.name,
             tiers: args.orgInput.tiers,
@@ -29,9 +28,7 @@ module.exports = {
 
         try {
             const orgSaveRes = await org.save();
-    
             const createdOrg = transformOrg(orgSaveRes);
-    
             const curUser = await User.findById(req.userId);
     
             if (!curUser) {
@@ -42,6 +39,33 @@ module.exports = {
             await curUser.save();
     
             return createdOrg;
+        } catch (err) {
+            throw err;
+        }
+    },
+
+    deleteOrg: async (args, req) => {
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated');
+        }
+
+        try {
+            const org = await Org.findById(args.orgId).populate('creator');
+
+            if (!org) {
+                throw new Error("Org does not exist");
+            }
+
+            await User.updateOne(
+                { _id: org.creator._id },
+                { $pull: { createdOrgs: args.orgId } }
+            )
+            
+            await Membership.deleteMany({ org: { _id: args.orgId } })
+
+            await org.deleteOne({ _id: args.orgId })
+
+            return transformOrg(org);
         } catch (err) {
             throw err;
         }
